dfd4a9e717a653f7c922c0d8417d7bb090d4a38b
Prathmesh <prathmeshc92@gmail.com>
2025-11-22 01:41:19 +0530

    Commit this

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("This is first code");
+		System.out.println("This is 2nd line of first code");
+
+	}
+
+}
